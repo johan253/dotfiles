68a6c6b675191fd03da4173156b9f2ee2aa2aeca
johan253 <johannjo2000@gmail.com>
2025-04-30 10:32:05 -0700

    Added neovim config files

---
+++ nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,38 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  lazy = false,
+  opts = {
+    filesystem = {
+      hijack_netrw_behavior = 'open_current',
+      use_libuv_file_watcher = false,
+      window = {
+        mappings = {
+          ['<leader>e'] = 'close_window',
+          ['l'] = 'open',
+          ['e'] = 'expand_all_subnodes',
+        },
+      },
+    },
+    event_handlers = {
+      {
+        event = 'file_open_requested',
+        handler = function()
+          require('neo-tree.command').execute { action = 'close' }
+        end,
+      },
+    },
+  },
+}
