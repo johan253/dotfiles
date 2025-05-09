10b3faf7ab04b01de4188405e946e82f1e92447b
johan253 <johannjo2000@gmail.com>
2025-05-09 09:04:54 -0700

    Added tmux config

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -11,7 +11,10 @@ return {
   -- import/override with your plugins folder
   { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.docker.lazydocker" },
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  {
+    import = "astrocommunity.markdown-and-latex.markdown-preview-nvim",
+    opts = { mkdp_port = "1111", mkdp_browser = "/usr/bin/google-chrome-stable" },
+  },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.pack.lua" },
