530da9be4c1840f0d1d323971a1fda51d6b7211a
johan253 <johannjo2000@gmail.com>
2025-07-08 18:28:27 -0700

    chore: add install script, adjust hyprland+waybar spacing

---
+++ install-arch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+yay -S hyprland \
+  hypridle \
+  hyprlock \
+  hyprpaper \
+  hyprshot \
+  hyprcwd \
+  wl-clipboard \
+  cliphist \
+  swaync \
+  waybar \
+  waybar-updates \
+  wofi \
+  kitty \
+  python-requests \
+  gnome-keyring \
+  vlc \
+  pavucontrol \
+  wireplumber \
+  nwg-look \
+  papirus-icon-theme
